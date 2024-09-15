068f0aabc9e9c3a70b4f245a27c5418cd6ae6f30
Andy Tyler <andrewjtyler1@gmail.com>
2024-09-15 14:28:28 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import "./tailwind.css";
+</script>
+
+<svelte:head>
+	<title>You App Name</title>
+	<link rel="stylesheet" href="/build/bundle.css" />
+</svelte:head>
+
+<main class="text-base w-32 h-32">
+	<div class="text-red-500">Hello</div>
+</main>
---
+++ src/content/content.ts
@@ -0,0 +1,6 @@
+export {};
+
+chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
+	console.log(message, chrome.runtime.getPlatformInfo);
+	return true;
+});
